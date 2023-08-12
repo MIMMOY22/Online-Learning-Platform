5ba63b9a2c34181b4870d31bea746619463c2002
MD. ATIKUR RAHMAN ( MIMMOY ) <mimmoyali2015@gmail.com>
2023-08-12 13:50:35 +0600

    initalldbtables

---
+++ OLP/OLP/DAL/EF/Models/Teacher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF.Models
+{
+    public class Teacher
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        [DefaultValue("teacher")]
+        public string type { get; set; }
+        public virtual ICollection<Course> courses { get; set; }
+        public virtual ICollection<Enrollments> enrollments { get; set; }
+        public virtual ICollection<Assignments> assignments { get; set; }
+        public Teacher()
+        {
+            courses = new List<Course>();
+            enrollments = new List<Enrollments>();
+            assignments = new List<Assignments>();
+        }
+    }
+}
